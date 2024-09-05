2b2fc18967dc64f666cb63924db5d6c2bf30f128
shogo.baseball0306@gmail.com <shogo.baseball0306@gmail.com>
2024-09-05 10:06:01 +0900

    hero

--- react-portfolio-2024/components/Hero.tsx
+++ react-portfolio-2024/components/Hero.tsx
@@ -1,4 +1,7 @@
+import { FaLocationArrow } from 'react-icons/fa'
+import ShimmerButton from './ui/ShimmerButton'
 import { Spotlight } from './ui/Spotlight'
+import { TextGenerateEffect } from './ui/TextGenerateEffect'
 
 function Hero() {
   return (
@@ -8,14 +11,34 @@ function Hero() {
             <Spotlight className="top-10 left-full h-[80vh] w-[50wh]" fill="purple" />
             <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]"  fill="blue"  />
         </div>
-        <div className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
-        {/* Radial gradient for the container to give a faded look */}
-        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-            <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
-                Backgrounds
-            </p>
+        <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.3] flex items-center justify-center absolute top-0 left-0">
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
         </div>
+        <div className='flex justify-center relative my-20 z-10'>
+            <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
+                <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>
+                    Website with modern technology
+                </h2>
 
+                <TextGenerateEffect 
+                className='text-center text-[40px] md:text-5xl lg:text-6xl' 
+                words='Thank you for visiting. Enjoy this special expedition.'
+                />
+
+                <p className='text-center'>
+                    My name is SHOGO. 
+                    This is my portfolio.
+                </p>
+
+                <a href="#about">
+                    <ShimmerButton 
+                    title = "Visit my works"
+                    icon={<FaLocationArrow />}
+                    position='right'
+                    />
+                </a>
+            </div>
+        </div>
     </div>
   )
 }
