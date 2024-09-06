681be9ec3a0198af35074557ad07f81d043ed0eb
shogo.baseball0306@gmail.com <shogo.baseball0306@gmail.com>
2024-09-06 18:16:27 +0900

    finish making bento section

--- react-portfolio-2024/data/index.ts
+++ react-portfolio-2024/data/index.ts
@@ -8,7 +8,7 @@ export const navItems = [
 export const gridItems = [
   {
     id: 1,
-    title: "I prioritize client collaboration, fostering open communication ",
+    title: "My major is law at a Japanese university, but I'm currently studying CS in the United States.",
     description: "",
     className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
     imgClassName: "w-full h-full",
@@ -33,6 +33,7 @@ export const gridItems = [
     className: "lg:col-span-2 md:col-span-3 md:row-span-2",
     imgClassName: "",
     titleClassName: "justify-center",
+    descriptionClassName: "whitespace-nowrap",
     img: "",
     spareImg: "",
   },
@@ -49,7 +50,7 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Currently building a JS Animation library",
+    title: "Currently building a Python Django Website",
     description: "The Inside Scoop",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
@@ -59,7 +60,7 @@ export const gridItems = [
   },
   {
     id: 6,
-    title: "Do you want to start a project together?",
+    title: "Are you interested in SHOGO KIKUCHI?",
     description: "",
     className: "lg:col-span-2 md:col-span-3 md:row-span-1",
     imgClassName: "",
