849255f8f4c9e2e57ff5e910cd006965be037880
shogo.baseball0306@gmail.com <shogo.baseball0306@gmail.com>
2024-09-15 06:24:35 -0700

    footer

--- react-portfolio-2024/app/page.tsx
+++ react-portfolio-2024/app/page.tsx
@@ -5,6 +5,7 @@ import RecentProjects from "@/components/RecentProjects";
 import { navItems } from "@/data";
 import Experience from "@/components/Experience";
 import Approach from "@/components/Approach";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -17,6 +18,7 @@ export default function Home() {
         <RecentProjects />
         <Experience />
         <Approach />
+        <Footer />
       </div>
     </main>
   );
