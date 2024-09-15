429f52425f54f0f04d4b81c18eed9e44f57c00a0
shogo.baseball0306@gmail.com <shogo.baseball0306@gmail.com>
2024-09-14 20:19:24 -0700

    approach section

--- react-portfolio-2024/components/Approach.tsx
+++ react-portfolio-2024/components/Approach.tsx
@@ -24,9 +24,7 @@ const Approach = () =>  {
         <Card 
         title="Enjoy Coding" 
         icon={<AceternityIcon order="Phase 2" />}
-        des="Once we agree on the plan, I cue my lofi playlist and dive into
-          coding. From initial sketches to polished code, I keep you updated
-          every step of the way."
+        des="I spend most of my day writing code, including self-study, lectures, and internships."
         >
           <CanvasRevealEffect
             animationSpeed={3}
@@ -35,17 +33,13 @@ const Approach = () =>  {
               [236, 72, 153],
               [232, 121, 249],
             ]}
-            dotSize={2}
+            dotSize={7}
           />
-          {/* Radial gradient for the cute fade */}
-          <div className="absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/50 dark:bg-black/90" />
         </Card>
         <Card 
-        title="Munni is Aditi" 
+        title="Success" 
         icon={<AceternityIcon order="Phase 3" />}
-        des="This is where the magic happens! Based on the approved design, 
-          I'll translate everything into functional code, building your website
-          from the ground up."
+        des="My unmatched perspicacity coupled with my sheer indefatigability makes me a feared opponent in any realm of human endeavour."
         >
           <CanvasRevealEffect
             animationSpeed={3}
@@ -74,7 +68,7 @@ const Card = ({
     <div
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
-      className="border border-black/[0.2] group/canvas-card flex items-center justify-center dark:border-white/[0.2]  max-w-sm w-full mx-auto p-4 relative h-[30rem] relative"
+      className="border border-black/[0.2] group/canvas-card flex items-center justify-center dark:border-white/[0.2]  max-w-sm w-full mx-auto p-4 lg:h-[35rem] relative rounded-3xl"
     >
       <Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
       <Icon className="absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black" />
@@ -94,11 +88,14 @@ const Card = ({
       </AnimatePresence>
 
       <div className="relative z-20">
-        <div className="text-center group-hover/canvas-card:-translate-y-4 group-hover/canvas-card:opacity-0 transition duration-200 w-full  mx-auto flex items-center justify-center">
+        <div className="text-center group-hover/canvas-card:-translate-y-4 group-hover/canvas-card:opacity-0 transition duration-200 w-full  mx-auto flex items-center justify-center absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]" >
           {icon}
         </div>
-        <h2 className="dark:text-white text-xl opacity-0 group-hover/canvas-card:opacity-100 relative z-10 text-black mt-4  font-bold group-hover/canvas-card:text-white group-hover/canvas-card:-translate-y-2 transition duration-200">
+        <h2 className="dark:text-white opacity-0 group-hover/canvas-card:opacity-100 relative z-10 text-black mt-4  font-bold group-hover/canvas-card:text-white group-hover/canvas-card:-translate-y-2 transition duration-20 text-center text-3xl">
           {title}
+        </h2>
+        <h2 className="text-sm dark:text-white  opacity-0 group-hover/canvas-card:opacity-100 relative z-10 text-black mt-4  font-bold group-hover/canvas-card:text-white group-hover/canvas-card:-translate-y-2 transition duration-200 text-center" style={{color: "#e4ecff"}}>
+          {des}
         </h2>
       </div>
     </div>
@@ -110,7 +107,7 @@ const AceternityIcon = ({order} : {order: string}) => {
     <div>
         <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
         <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-        <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+        <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-5 py-2  text-white backdrop-blur-3xl text-2xl font-bold">
             {order}
         </span>
         </button>
